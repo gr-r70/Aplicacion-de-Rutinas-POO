2c113c281bbdd1f608cbe571485b6455e32b932a
cristvxnnn <cristian.torres_m@uao.edu.co>
2026-05-17 18:56:59 -0500

    correccion de escritura en IGestionable

--- src/main/java/Modelo/IGestionable.java
+++ src/main/java/Modelo/IGestionable.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Modelo;
+package modelo;
 import java.util.ArrayList;
 public interface IGestionable{
     void agregar(Rutina r);
     void eliminar (int indice);
     ArrayList<Rutina> getRutinas();
-    String lista();
+    String listar();
 }
 
