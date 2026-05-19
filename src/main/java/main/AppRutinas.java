c8ba1c37f87313dbacd2f4f5abbed086e084a4cf
davidstepajaro <david_ste.pajaro@uao.edu.co>
2026-05-19 18:51:56 -0500

    Corrección: configuración de clase principal

--- src/main/java/main/AppRutinas.java
+++ src/main/java/main/AppRutinas.java
@@ -2,15 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.apprutinas;
+package main;
 
-/**
- *
- * @author Usuario
- */
+import vista.VentanaPrincipal;
+
 public class AppRutinas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new VentanaPrincipal();
+        });
     }
 }
